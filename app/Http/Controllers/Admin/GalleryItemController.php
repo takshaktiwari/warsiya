dc6d6f59236e57a4c63e4f8c2e77fe6c9352f1dc
Takshak Tiwari <takshaktiwari@gmail.com>
2023-03-20 21:17:59 +0530

    gallery delete fixed

--- app/Http/Controllers/Admin/GalleryItemController.php
+++ app/Http/Controllers/Admin/GalleryItemController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+use Takshak\Agallery\Models\GalleryItem;
 use Takshak\Agallery\Traits\Controllers\Admin\GalleryItemControllerTrait;
 
 class GalleryItemController extends Controller
 {
     use GalleryItemControllerTrait;
+
+    public function destroy(GalleryItem $galleries_item)
+    {
+        try {
+            Storage::disk('public')->delete([
+                $galleries_item->image_lg,
+                $galleries_item->image_md,
+                $galleries_item->image_sm
+            ]);
+        } catch (\Exception $e) {
+            logger($e->getMessage());
+        }
+        $galleries_item->delete();
+        return to_route('admin.galleries-items.index')->withSuccess('SUCCESS !! Gallery item has been successfully deleted.');
+    }
 }
